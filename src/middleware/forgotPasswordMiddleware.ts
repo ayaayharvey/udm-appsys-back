69943ddc26d39b9700a8e5e742dd635e40354f00
ayaayharvey <ayaayharvey@gmail.com>
2023-12-17 14:05:37 +0800

    fix bug

--- src/middleware/forgotPasswordMiddleware.ts
+++ src/middleware/forgotPasswordMiddleware.ts
@@ -1,20 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import database from "../config/postgresql";
 
-export async function forgotPasswordMiddleware (req: Request, res: Response, next: NextFunction) {
-    const {email} = req.body;
-    const query = `SELECT * FROM user_account_tbl WHERE user_email = $1`;
-    const value = [email];
-    try {
-        const client = await database.connect();
-        const result = await client.query(query, value);
-        client.release();
-        if (result.rowCount <= 0) {
-            return res.status(409).send({status: "Email doesn't exist"});
-        }
-        next();
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
+export async function forgotPasswordMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { email } = req.body;
+  const query = `SELECT * FROM user_account_tbl WHERE user_email = $1`;
+  const value = [email];
+  try {
+    const client = await database.connect();
+    const result = await client.query(query, value);
+    client.release();
+    if (result && result.rowCount && result.rowCount <= 0) {
+      return res.status(409).send({ status: "Email doesn't exist" });
     }
-} 
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+}
